b8f773b6105c14313136c2ffc7cc63059e5fa884
mikesoriano <michael.soriano@live.com>
2019-06-01 11:34:28 -0700

    deleted users.js

--- src/Users.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-class Users extends React.Component {
-  render() {
-    return <div className="users">
-          <h1>User</h1>
-        </div>
-  }
-}
-export default Users
