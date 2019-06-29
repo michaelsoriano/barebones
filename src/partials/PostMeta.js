a88a6d1136463b76cd75c46bbeeca279bf4e9d77
mikesoriano <michael.soriano@live.com>
2019-06-29 13:11:03 -0700

    removed meta from post type page

--- src/partials/PostMeta.js
+++ src/partials/PostMeta.js
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import WithConsumer from '../context/WithConsumer';
 
@@ -7,12 +6,16 @@ const PostMeta = ({index,context}) => {
  
     const posts = () => context.posts;
     const item = posts()[index];
-    console.log(item);
+     
+    let theMeta = ''; 
+    if(item.type == 'post'){
+        theMeta = (<div className="post-meta">
+        Published:  <Moment format="MM/DD/YYYY">{item.date}</Moment>, 
+        Written by {item.author_name}, Under {item.category_name}
+        </div>)
+    }
 
-    return (
-        <div className="post-meta">
-            Published:  <Moment format="MM/DD/YYYY">{item.date}</Moment>, 
-        </div>);
+    return theMeta;
 
 };
 export default WithConsumer(PostMeta);
